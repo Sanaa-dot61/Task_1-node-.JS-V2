ed72e2232e6faba50dd338fe1e418e79e1e6b8e8
Sanaa Mohamed <sanaamohamed491@gmail.com>
2026-08-29 15:44:21 +0300

    Update allData.js

--- allData.js
+++ allData.js
@@ -1,12 +1,1 @@
-const firstName = "Sanaa";
-const lastName = "Mohamed";
-const city = "Cairo";
-function mul(x, y) {
-    console.log(x * y);
-}
-module.exports = {
-    fname: firstName,
-    lname: lastName,
-    city: city,
-    fun1: mul
-};
+
