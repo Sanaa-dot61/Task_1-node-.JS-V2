8826a8ef631f2915231c9ab9d89d88396711d9a7
Sanaa Mohamed <sanaamohamed491@gmail.com>
2026-08-29 15:37:24 +0300

    Refactor data management functions in data10.js

--- data10.js
+++ data10.js
@@ -1,102 +1,103 @@
-
-
-  const fs = require("fs")
-
-  const addPerson = (id , fname , lname , city , age) => {
-       const allData = loadInfo()
-
-       const duplicatedData = allData.filter((obj) =>{
-          return  obj.id === id 
-       })
-
-        if (duplicatedData.length == 0  ){
-
-       allData.push({
-         id : id,
-         fname : fname,
-         lname : lname,
-         city : city,
-         age : age
-       })
-       savealldata(allData)
-      } else {
-        console.log("ERROR DUPLICATED DATA")
-      }
-  }
-
-/////////////////////////////////////////////////////////////////////
-
-  const loadInfo = () => {
-    try { 
-        const dataJson = fs.readFileSync("data10.json").toString()
-        return  JSON.parse(dataJson)
+const fs = require("fs");
+// Load data from JSON file
+const loadInfo = () => {
+    try {
+        const dataJson = fs.readFileSync("data10.json").toString();
+        return JSON.parse(dataJson);
     }
     catch {
-        return []
+        return [];
     }
-     
-  }
-
-/////////////////////////////////////////////////////////////////////
- 
-  const savealldata = (allData) => {
-     const saveallDataJson  = JSON.stringify(allData)
-     fs.writeFileSync("data10.json" , saveallDataJson)
-  }
-
-///////////////////////////////////////////////////////////////////////
-
-// delete data :
-
-   const deleteData  = (id) =>{
-      const allData = loadInfo()
-
-       const dataTokeep = allData.filter((obj) => {
-          return obj.id !== id
-       })
-
-      //  console.log(dataTokeep)
-      //  console.log("you have successfully deleted an item")
-
-       savealldata(dataTokeep)
-      
-   }
-
-  ////////////////////////////////////////////////////////////////////////////////////////
-
-  // to read data :
-
-    const readData = (id) => {
-        const allData = loadInfo()
-
-        const itemNeeded = allData.find((obj) => {
-           return  obj.id == id 
-        })
-
-        // console.log(itemNeeded)
-        if (itemNeeded) {
-          console.log(itemNeeded)
-        }else {
-          console.log("ID NEEDED NOT FOUND")
-        }
+};
+// Save data to JSON file
+const saveAllData = (allData) => {
+    const saveAllDataJson = JSON.stringify(allData);
+    fs.writeFileSync("data10.json", saveAllDataJson);
+};
+// Add person
+const addPerson = (id, fname, lname, city, age) => {
+    const allData = loadInfo();
+    const duplicatedData = allData.filter((obj) => {
+        return obj.id === id;
+    });
+    if (duplicatedData.length === 0) {
+        allData.push({
+            id: id,
+            fname: fname,
+            lname: lname,
+            city: city,
+            age: age
+        });
+        saveAllData(allData);
+        console.log("Person added successfully");
+    } 
+    else {
+        console.log("ERROR: DUPLICATED ID");
     }
-  
-////////////////////////////////////////////////////////////////////////////////////////////
-
-// list 
-          const listData = () => {
-             const  allData = loadInfo()
-
-              allData.forEach((obj) => {
-                console.log(obj.fname , obj.city)
-              })
-
-          }
-       
-
+};
+// Delete specific person
+const deleteData = (id) => {
+    const allData = loadInfo();
+    const dataToKeep = allData.filter((obj) => {
+        return obj.id !== id;
+    });
+    if (dataToKeep.length === allData.length) {
+        console.log("ID NOT FOUND");
+    } 
+    else {
+        saveAllData(dataToKeep);
+        console.log("Person deleted successfully");
+    }
+};
+// Delete all people
+const deleteAllData = () => {
+    saveAllData([]);
+    console.log("All people deleted successfully");
+};
+// Read specific person
+const readData = (id) => {
+    const allData = loadInfo();
+    const itemNeeded = allData.find((obj) => {
+        return obj.id == id;
+    });
+    if (itemNeeded) {
+        console.log(itemNeeded);
+    } 
+    else {
+        console.log("ID NEEDED NOT FOUND");
+    }
+};
+// List all people
+const listData = () => {
+    const allData = loadInfo();
+    if (allData.length === 0) {
+        console.log("NO DATA FOUND");
+        return;
+    }
+    allData.forEach((obj) => {
+        console.log(
+            `ID: ${obj.id} | Name: ${obj.fname} ${obj.lname} | Age: ${obj.age} | City: ${obj.city}`
+        );
+    });
+};
+// List full name and city
+const listNamesAndCities = () => {
+    const allData = loadInfo();
+    if (allData.length === 0) {
+        console.log("NO DATA FOUND");
+        return;
+    }
+    allData.forEach((obj) => {
+        console.log(
+            `Full Name: ${obj.fname} ${obj.lname} | City: ${obj.city}`
+        );
+    });
+};
 module.exports = {
     addPerson,
     deleteData,
+    deleteAllData,
     readData,
-    listData
-}
+    listData,
+    listNamesAndCities
+};
